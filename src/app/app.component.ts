cc77bd47a967aedac811ce2d1d5dd1326563ca95
zebesta <chris.sebesta@gmail.com>
2016-09-07 17:22:49 -0400

    got to work by updating directives and imports

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,10 @@
 import { Component } from '@angular/core';
+import { PlantsComponent } from './plants/plants.component';
+import { TestComponent } from './test/test.component';
 
 @Component({
   moduleId: module.id,
+  directives: [TestComponent, PlantsComponent],
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css']
--- src/system-config.ts
+++ src/system-config.ts
@@ -39,6 +39,7 @@ const barrels: string[] = [
   'app/plant-detail',
   'app/plants',
   'app/dashboard',
+  'app/test',
   /** @cli-barrel */
 ];
 
